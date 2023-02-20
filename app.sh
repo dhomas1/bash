d6349f60b674d450a6704af0c1fb11d3c0630d62
dhomas1 <dom@cotugno.co>
2023-02-20 13:16:12 -0500

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,18 +6,18 @@ local FILE="${FOLDER}.tar.gz"
 local URL="http://ftp.gnu.org/gnu/bash/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-for n in {001..015}; do
-  if [[ ! -f "${PWD}/download/bash-${VERSION}-${n}.patch" ]]; then
-    wget -O "${PWD}/download/bash-${VERSION}-${n}.patch" "http://ftp.gnu.org/gnu/bash/bash-5.2-patches/bash52-${n}"
-  fi
-done
+#for n in {001..015}; do
+#  if [[ ! -f "${PWD}/download/bash-${VERSION}-${n}.patch" ]]; then
+#    wget -O "${PWD}/download/bash-${VERSION}-${n}.patch" "http://ftp.gnu.org/gnu/bash/bash-5.2-patches/bash52-${n}"
+#  fi
+#done
 
-for f in ${PWD}/download/bash-${VERSION}-*.patch; do
-  pushd "target/${FOLDER}"
-  echo "${f}"
-  patch -p0 -i "${f}"
-  popd
-done
+#for f in ${PWD}/download/bash-${VERSION}-*.patch; do
+#  pushd "target/${FOLDER}"
+#  echo "${f}"
+#  patch -p0 -i "${f}"
+#  popd
+#done
 
 pushd "target/${FOLDER}"
 ./configure --host="${HOST}" --prefix="${DEST}" --mandir="${DEST}/man"
